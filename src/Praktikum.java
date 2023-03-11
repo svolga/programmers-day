37ce75fda358e6ac8a08daca4221c0436be82776
Ракчеев Сергей <rakcheev.sg@magnat.ru>
2023-03-11 10:09:11 +0300

    first commit

---
+++ src/Praktikum.java
@@ -0,0 +1,35 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Praktikum {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите год:");
+        int year = scanner.nextInt();
+
+        boolean isLeapYear = isLeapYear(year);
+
+        int days = 255;
+        LocalDate date = LocalDate.of(year,1,1);
+        LocalDate dateDeveloper = date.plusDays(days);
+
+        if (isLeapYear(year)) {
+            System.out.println("День программиста в високосный год: "+dateDeveloper.format(DateTimeFormatter.ofPattern("dd.MM.uuuu")));
+        }
+        else{
+            System.out.println("День программиста в НЕ високосный год: "+dateDeveloper.format(DateTimeFormatter.ofPattern("dd.MM.uuuu")));
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (0 == year%400)
+            return true;
+        else if (0 == year%100)
+            return false;
+
+        return 0 == year%4;
+    }
+}
